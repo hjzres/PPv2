65e7c169da3b1cc003a4dbd7f04069aac63fff1b
Andrei <142443853+hjzres@users.noreply.github.com>
2024-04-02 12:33:53 -0400

    The player can move, but it doesn't look good

---
+++ Assets/Scripts/Player/PlayerCam.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player{
+	public class PlayerCam : MonoBehaviour
+	{
+		public float Sense;
+		
+		[SerializeField] Transform orientation;
+		
+		private float _xRotation;
+		private float _yRotation;
+		
+		void Awake()
+		{
+			Cursor.lockState = CursorLockMode.Locked;
+			Cursor.visible = false;
+		}
+		
+		void Update()
+		{
+			float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * Sense;
+			float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * Sense;
+			
+			_yRotation += mouseX;
+			_xRotation -= mouseY;
+			
+			_xRotation = Mathf.Clamp(_xRotation, -90f, 90f);
+			
+			transform.rotation = Quaternion.Euler(_xRotation, _yRotation, 0);
+			orientation.rotation = Quaternion.Euler(0, _yRotation, 0);
+		}
+	}
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,22 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Callbacks;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
 namespace Player
 {
 	public class PlayerMovement : MonoBehaviour
 	{
-		PlayerInput playerInput;
-		InputAction moveAction; 
-		// Start is called before the first frame update
+		PlayerInput _playerInput;
+		InputAction _moveAction;
+		
+		[SerializeField] float speed;
+		[SerializeField] Transform orientation;
+		
+		private Rigidbody _rb;
+		private Vector3 _moveDirection;
+		
 		void Start()
 		{
-			playerInput = GetComponent<PlayerInput>();
-			moveAction = playerInput.actions.FindAction("Move");
+			_playerInput = GetComponent<PlayerInput>();
+			_moveAction = _playerInput.actions.FindAction("Move");
+			_rb = GetComponent<Rigidbody>();
 		}
 
-		// Update is called once per frame
 		void Update()
 		{
 			MovePlayer();
@@ -24,7 +32,9 @@ namespace Player
 		
 		void MovePlayer()
 		{
-			Debug.Log(moveAction.ReadValue<Vector2>());
+			Vector2 direction = _moveAction.ReadValue<Vector2>();
+			_moveDirection = orientation.forward * direction.y + orientation.right * direction.x;
+			_rb.AddForce(_moveDirection.normalized * speed * 10f, ForceMode.Force);
 		}
 	}
 }
